773e3aeb08962cafa7155f02a807edbc29158c02
Anthony Bustamante <rbustama@andrew.cmu.edu>
2024-02-02 18:56:09 -0500

    Second test commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <div style={{ display: 'flex', height: '100vh', width: '100%' }}>
       <div style={{ flex: 1 }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }} 
           defaultCenter={center}
           defaultZoom={zoom}
           yesIWantToUseGoogleMapApiInternals
